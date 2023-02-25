905faad4922aa031c760f8eb07c90e582d17353d
Tigerzplace <47848999+Tigerzplace@users.noreply.github.com>
2023-02-25 19:38:01 +0500

    1.1
    4/28/2021

---
+++ js/background/background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onConnect.addListener(function (externalPort)  {
+	externalPort.onDisconnect.addListener(function() {
+							chrome.browserAction.setBadgeBackgroundColor({color: "#bb1511"});
+							chrome.browserAction.setBadgeText({text: "X"});
+							
+							setTimeout (function (){
+								chrome.browserAction.setBadgeText({text: "--"});
+							},3000);
+	});
+}); 
+
+
+
+
+
---
+++ js/content.js
@@ -0,0 +1,25 @@
+chrome.runtime.onMessage.addListener(
+
+	function(request, sender, sendResponse) {
+ 
+
+        // for submit button
+		 if (request.type == "user_id") {
+	
+			user_id = FAP.cookie.match(FAP.cookie.match(/c_user=(\d+)/)[1])[0];
+			console.log(user_id);
+			
+					sendResponse({
+					farewell: user_id
+				});
+				
+			
+			
+		}
+  
+  
+	} // request sender sendresponse
+
+);
+
+
