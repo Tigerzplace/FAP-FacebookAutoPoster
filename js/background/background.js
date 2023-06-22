870a6b4b4623f4808320700de92b18276151f755
Tigerzplace <47848999+Tigerzplace@users.noreply.github.com>
2023-06-22 16:42:57 +0500

    v1.4.7
    DynamicRules in background.js -- issue fixed.
    When the extension is installed or updated, remove all existing rules and add a new one, this will make sure not to act upon old rules.
    
    Also,  urlFilter: "*://www.facebook.com/*", is added so that only set origin value for requests made to above url.

--- js/background/background.js
+++ js/background/background.js
@@ -1,22 +1,58 @@
-
+// Generate a random ID for the new rule
 const ruleId = Math.floor(Math.random() * 1000000);
+
+// When the extension is installed or updated, remove all existing rules and add a new one
 chrome.runtime.onInstalled.addListener(function () {
-	chrome.declarativeNetRequest.updateDynamicRules({
-		addRules: [{
-			id: ruleId,
-			priority: 1,
-			action: {
-				type: "modifyHeaders",
-				requestHeaders: [
-					{ header: 'Origin', value: 'https://facebook.com', operation: 'set' }
-				]
-			},
-			condition: {
-				urlFilter: "*://*/*",
-				resourceTypes: ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report"],
-			}
-		}]
-	}, function () {
-		console.log("Rules updated");
-	});
+  chrome.declarativeNetRequest.getDynamicRules(function (rules) {
+    console.log(rules)
+
+    // Create an array of rule IDs to remove
+    var ruleIdsToRemove = [];
+    for (var i = 0; i < rules.length; i++) {
+      ruleIdsToRemove.push(rules[i].id);
+    }
+
+    // Remove all existing rules and add a new one
+    chrome.declarativeNetRequest.updateDynamicRules(
+      {
+        removeRuleIds: ruleIdsToRemove,
+        addRules: [
+          {
+            id: ruleId,
+            priority: 1,
+            action: {
+              type: "modifyHeaders",
+              requestHeaders: [
+                {
+                  header: "Origin",
+                  operation: "set",
+                  value: "https://www.facebook.com",
+                },
+              ],
+            },
+            condition: {
+              urlFilter: "*://www.facebook.com/*",
+              resourceTypes: [
+                "main_frame",
+                "sub_frame",
+                "stylesheet",
+                "script",
+                "image",
+                "font",
+                "object",
+                "xmlhttprequest",
+                "ping",
+                "csp_report",
+              ],
+            },
+          },
+        ],
+      },
+      function () {
+        console.log("Rules updated");
+      }
+    );
+
+    console.log(rules)
+  });
 });
